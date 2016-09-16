c7211b91c67f5b2def92bcd95c16722ab4c71e6d
BohdanKul <bohdan.kul@gmail.com>
2016-09-16 11:49:23 -0400

    Reworked previous implementation of the merge clusters algorithm. The previous algorithm was wrong. Also added another implementation based on boost disjoint set datastructure. However, this implementation doesn't give correct results. So it is left commented out for now.

--- clusterbuilder.h
+++ clusterbuilder.h
@@ -2,11 +2,14 @@
 #define CLUSTERBUILDER_H
 
 #include <set>  // used in Merge Clusters routine
+#include <list>
 
 #include <boost/random/uniform_real.hpp>
 #include <boost/random/uniform_int.hpp>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/variate_generator.hpp>
+#include <boost/pending/disjoint_sets.hpp>
+#include <boost/property_map/property_map.hpp>
 
 #include "simulationcell.h"
 //#include "spins.h"
@@ -14,11 +17,13 @@
 
 
 using namespace std;
+namespace boo=boost;
 
 class ClusterBuilder{
     protected:
         double P;
         int signJ ;
+        int nc;
         SimulationCell& SC;
         vector<int> Cluster_Partition;
         boost::variate_generator<boost::mt19937&, boost::uniform_real<double>>& RealRnd;
@@ -27,9 +32,11 @@ class ClusterBuilder{
                        boost::variate_generator<boost::mt19937&, boost::uniform_real<double> >& _RealRnd);
 
         void ResetPartition();
-            
+        void TraceCluster(int nspin, bool toFlip);
         bool TraceCluster(int ncluster, int nspin, bool toFlip);
         int  MergeClusters(vector<pair<int, int>>& Boundary);
+        int  MergeClusters2(vector<pair<int, int>>& Boundary);
+        int  GetClustersN(){ return nc;};
 };
 
 
@@ -52,6 +59,15 @@ ClusterBuilder::ClusterBuilder(SimulationCell& _SC, double _beta, int _signJ,
 void ClusterBuilder::ResetPartition(){
     // reset to default values 
     fill(Cluster_Partition.begin(), Cluster_Partition.end(), -1);
+    nc = 0;
+}
+
+/****************************************************************************************************
+ 
+ 
+ ***************************************************************************************************/
+void ClusterBuilder::TraceCluster(int nspin, bool toFlip){
+     if (TraceCluster(nc, nspin, toFlip)) nc++;
 }
 
 /****************************************************************************************************
@@ -84,58 +100,152 @@ bool ClusterBuilder::TraceCluster(int ncluster, int nspin, bool toFlip){
     return true;
 }
 
+
+
+/****************************************************************************************************
+ * ----------------------------------------!!!BROKEN!!!----------------------------------------------
+ *  ---------------------------------- use on your own risk -----------------------------------------
+ * Given a vector of cluster indices associated with spins as well as a vector of spin pairs attached 
+ * by the boundary, the function  counts how many clusters exist on the boundary for such a connection.
+ ***************************************************************************************************/
+//int ClusterBuilder::MergeClusters(vector<pair<int, int> > &Boundary){
+//    typedef map<int, size_t> rank_t;
+//    typedef map<int, int>  parent_t;
+//    rank_t   rank_map;
+//    parent_t parent_map;
+//
+//    boo::associative_property_map<   rank_t>   rank_pmap(  rank_map);
+//    boo::associative_property_map< parent_t> parent_pmap(parent_map);
+//    boo::disjoint_sets< boo::associative_property_map<rank_t>,
+//                        boo::associative_property_map<parent_t> > ds(rank_pmap, parent_pmap);
+//
+//     cout << "   ----- Cluster Merge ----- " << endl << endl;
+//     cout << "   Boundary: " << endl;
+//     for (auto pspins=Boundary.begin(); pspins!=Boundary.end(); pspins++)
+//         cout << "   (" << pspins->first << "," << pspins->second << ")" << endl;
+//     
+//     cout << "   Cluster partition: " << endl;
+//     for (auto cp=Cluster_Partition.begin(); cp!=Cluster_Partition.end(); cp++)
+//         cout << *cp << " ";
+//     cout << endl;
+//    
+//     
+//     vector<int> dv; 
+//    for (auto i=0; i!=GetClustersN(); i++){
+//        ds.make_set(i);
+//        dv.push_back(i);
+//    }
+//    cout << "Before: " << ds.count_sets(dv.begin(), dv.end()) << endl;
+//
+//    for (auto sp=Boundary.begin(); sp!=Boundary.end(); sp++){
+//        ds.union_set(sp->first, sp->second); 
+//    }
+//    cout << "After:  " << ds.count_sets(dv.begin(), dv.end()) << endl;
+//
+//    return ds.count_sets(dv.begin(), dv.end());
+//}
+
+
+
+
 /****************************************************************************************************
  * Given a vector of cluster indices associated with spins as well as a vector of spin pairs attached 
  * by the boundary, the function  counts how many clusters exist on the boundary for such a connection.
  ***************************************************************************************************/
 int ClusterBuilder::MergeClusters(vector<pair<int, int>>& Boundary){
-     int bclus; int uclus;
+    int bclus; int uclus;
 
-     vector< set<int> > MergedClusters; 
-     bool new_merge;
-     
-     cout << "   ----- Cluster Merge ----- " << endl << endl;
-     cout << "   Boundary: " << endl;
-     for (auto pspins=Boundary.begin(); pspins!=Boundary.end(); pspins++)
-         cout << "   (" << pspins->first << "," << pspins->second << ")" << endl;
-     
-     cout << "   Cluster partition: " << endl;
-     for (auto cp=Cluster_Partition.begin(); cp!=Cluster_Partition.end(); cp++)
-         cout << *cp << " ";
-     cout << endl;
+    list< set<int> > MergedClusters; 
+    bool new_cluster_b;
+    bool new_cluster_u;
+    //set<int>* cluster_b;
+    //set<int>* cluster_u;
+    list<set<int>>::iterator cluster_b;
+    list<set<int>>::iterator cluster_u;
 
-     for (auto Spin_Pair=Boundary.begin(); Spin_Pair!=Boundary.end(); Spin_Pair++){
-         bclus = Cluster_Partition[ Spin_Pair->first  ];
-         uclus = Cluster_Partition[ Spin_Pair->second ];
-         
-         if (bclus != uclus){
-             new_merge = true;
-             
-             for (auto mcluster = MergedClusters.begin(); mcluster != MergedClusters.end(); mcluster++){
-                 if (mcluster->find(bclus) != mcluster->end()){
-                     mcluster->insert(uclus); 
-                     new_merge = false;
-                     break;
-                 }
-                 if (mcluster->find(uclus) != mcluster->end()){
-                     mcluster->insert(bclus); 
-                     new_merge = false;
-                     break;
-                 }
+    //cout << "   ----- Cluster Merge ----- " << endl << endl;
+    //cout << "   Boundary: " << endl;
+    //for (auto pspins=Boundary.begin(); pspins!=Boundary.end(); pspins++)
+    //    cout << "   (" << pspins->first << "," << pspins->second << ")" << endl;
+    //
+    //cout << "   Cluster partition: " << endl;
+    //for (auto cp=Cluster_Partition.begin(); cp!=Cluster_Partition.end(); cp++)
+    //    cout << *cp << " ";
+    //cout << endl;
+
+    for (auto Spin_Pair=Boundary.begin(); Spin_Pair!=Boundary.end(); Spin_Pair++){
+        bclus = Cluster_Partition[ Spin_Pair->first  ];
+        uclus = Cluster_Partition[ Spin_Pair->second ];
+        
+        if (bclus == uclus){
+           new_cluster_b = true;
+
+           for (auto mcluster = MergedClusters.begin(); mcluster != MergedClusters.end(); mcluster++){
+               if (mcluster->find(bclus) != mcluster->end()){
+                   new_cluster_b = false;
+                   break;
+               }
+           } 
+        
+           if (new_cluster_b){
+              set<int> tset;
+              tset.insert(bclus);
+              MergedClusters.push_back(tset); 
+           }
+        }
+        else{
+            new_cluster_b = true;
+            new_cluster_u = true;
+
+            for (auto mcluster = MergedClusters.begin(); mcluster != MergedClusters.end(); mcluster++){
+                if (mcluster->find(bclus) != mcluster->end()){ new_cluster_b = false; cluster_b = mcluster;}
+                if (mcluster->find(uclus) != mcluster->end()){ new_cluster_u = false; cluster_u = mcluster;}
+               
+                if ( (not(new_cluster_b)) and (not(new_cluster_u)) ) break;
+
              }
-             
-             if (new_merge){
-                set<int> tset;
-                tset.insert(bclus);
-                tset.insert(uclus);
-                MergedClusters.push_back(tset); 
-             }
+        
+            if       ((new_cluster_b) and not(new_cluster_u)) cluster_u->insert(bclus);
+            else if  ((new_cluster_u) and not(new_cluster_b)) cluster_b->insert(uclus);
+            else if  (new_cluster_u and new_cluster_b){
+                     set<int> tset;
+                     tset.insert(bclus);
+                     tset.insert(uclus);
+                     MergedClusters.push_back(tset); 
+            }
+            else if  (cluster_b != cluster_u){
+                     if (cluster_u->size() > cluster_b->size()){
+                         cluster_u->insert(cluster_b->begin(), cluster_b->end());
+                         //list<set<int>>::iterator it(cluster_b);
+                         MergedClusters.erase(cluster_b);
+                     }
+                     else{
+                         cluster_b->insert(cluster_u->begin(), cluster_u->end());
+                         //list<set<int>>::iterator it(cluster_u);
+                         MergedClusters.erase(cluster_u);
+                     }
+
+            }
+
         }
     }
     
+    //int i = 0;
+    //cout << "   Merged clusters: " << endl;
+    //for (auto iset  = MergedClusters.begin();
+    //          iset != MergedClusters.end();
+    //          iset ++){
+    //    cout << "   #" << i << ": ";
+    //    for (auto ic  = iset->begin();
+    //              ic != iset->end();
+    //              ic ++){
+    //        cout << *ic << " ";
+    //    }
+    //    cout << endl;
+    //    i++;
+    //}
     int NC =  MergedClusters.size();
-    
-    cout << "   # clusters = " << NC << endl;
+    //cout << "   # clusters = " << NC << endl;
 
     return NC;
 }
